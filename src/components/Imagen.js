633a577c0ae05dcb48b36eed66ce5a4426cfd8ea
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2021-01-26 12:34:28 +0100

    correccion de errores 2

--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Imagen = ({imagen}) => {
 
   // extraer las variables con destructuring
-  const {largeImageURL, likes, previewURL, tags, views, userImageURL} = imagen;
-
+  const {largeImageURL, likes, previewURL, tags, views} = imagen;
+  
   return ( 
     <div className='col-12 col-sm-6 col-md-4 col-lg-3 mb-4'>
       <div className='card'>
         <img 
           src={previewURL} 
-          alt={tags} className='card-img-top'
+          alt={tags} 
           className='card-img-top'
         />
         <div className='card-body'>
